dfca14a5d61fc52cf989fd2846f664903741de4e
zhangmin <1464253423@qq.com>
2023-05-04 22:59:44 +0800

    销售趋势图表和排行榜开发完成100%

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="SalesView">
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
       <template #header>
         <div class="menu-wrapper">
           <el-menu mode="horizontal" :default-active="activeIndex" @select="onMenuSelect" class="sales-view-el-menu">
@@ -28,7 +28,20 @@
         </div>
       </template>
       <template>
-        <div>222</div>
+        <div class="sales-view-chart-wrapper">
+          <v-chart :option="option"></v-chart>
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div class="list-item" v-for="item in rankData" :key="item.no">
+                <!-- <div class="list-item-no" :class="+item.no <= 3 ? 'top-no' : ''">{{ item.no }}</div> -->
+                <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{ item.no }}</div>
+                <div class="list-item-name">{{ item.name }}</div>
+                <div class="list-item-money">{{ item.money }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </el-card>
   </div>
@@ -73,6 +86,99 @@ export default {
             picker.$emit('pick', [start, end])
           }
         }]
+      },
+      rankData: [
+        {
+          no: 1,
+          name: '塔斯汀',
+          money: '323,234'
+        },
+        {
+          no: 2,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 3,
+          name: '必胜客',
+          money: '323,234'
+        },
+        {
+          no: 4,
+          name: '赛百味',
+          money: '323,234'
+        },
+        {
+          no: 5,
+          name: '麦当劳',
+          money: '323,234'
+        },
+        {
+          no: 6,
+          name: '牛耕记',
+          money: '323,234'
+        },
+        {
+          no: 7,
+          name: '华莱士',
+          money: '323,234'
+        }
+      ],
+      option: {
+        color: '#3398DB',
+        title: {
+          text: '年度销售额',
+          textStyle: {
+            fontSize: 12,
+            color: '#666'
+          },
+          left: 25,
+          top: 20
+        },
+        grid: {
+          top: 70,
+          left: 60,
+          right: 60,
+          bottom: 50
+        },
+        xAxis: {
+          type: 'category',
+          axisLine: {
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          axisLabel: {
+            color: '#333'
+          },
+          axisTick: {
+            // show: false // 隐藏短竖线
+            alignWithLabel: true, // 短竖线靠近label
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+        },
+        yAxis: {
+          axisLine: {
+            show: false // 隐藏y轴
+          },
+          axisTick: {
+            show: false // 隐藏短竖线
+          },
+          splitLine: {
+            lineStyle: {
+              type: 'dotted',
+              color: '#eee'
+            }
+          }
+        },
+        series: [{
+          type: 'bar',
+          barWidth: '35%',
+          data: [400, 70, 180, 350, 380, 200, 250, 300, 350, 300, 400, 250]
+        }]
       }
     }
   },
@@ -109,6 +215,59 @@ export default {
       // justify-content: flex-end;
       .sales-view-date-picker{
         margin-left: 20px;
+      }
+    }
+  }
+  .sales-view-chart-wrapper{
+    display: flex;
+    height: 270px;
+    .echarts{
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    .sales-view-list{
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      .sales-view-title{
+        margin-top: 20px;
+        font-size: 12px;
+        font-weight: 500;
+        color: #666;
+      }
+      .list-item-wrapper{
+        margin-top: 15px;
+        .list-item{
+          display: flex;
+          align-items: center;
+          font-size: 12px;
+          height: 20px;
+          padding: 6px 20px 6px 0;
+          .list-item-no{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 20px;
+            height: 20px;
+            color: #333;
+            &.top-no{
+              color: #fff;
+              font-weight: 500;
+              background: #000;
+              border-radius: 50%;
+            }
+          }
+          .list-item-name{
+            margin-left: 10px;
+            color: #333;
+          }
+          .list-item-money{
+            flex: 1;
+            text-align: right;
+          }
+        }
       }
     }
   }
